af0489137f5a5928fcba8ee579849547f368609c
wangyuanli <yuanli.wang@xiaozao.org>
2018-03-21 15:05:43 +0800

    add .

--- test/test.js
+++ test/test.js
@@ -9,22 +9,26 @@ const assert = require('assert');
 const RegExps = require('..');
 
 describe('正则表达式', () => {
-
   it('匹配手机号码', () => {
     [
       '13800000000',
-      '14712341234',
-      '15012341234',
-    ].forEach((mobile) => {
-      assert.ok(RegExps.mobile.test(mobile));
-    });
+      '15712341234',
+      '17012341234',
+      '18012341234'
+    ].forEach(
+      mobile => {
+        assert.ok(RegExps.mobile.test(mobile));
+      }
+    );
     [
+      '1873482665',
       '23800000000',
       '1471234l234',
       '147000000000',
       '150-123-41234',
       '150-1234-1234',
-    ].forEach((mobile) => {
+      '150.1234.1234'
+    ].forEach(mobile => {
       assert.ok(!RegExps.mobile.test(mobile));
     });
   });
@@ -33,15 +37,15 @@ describe('正则表达式', () => {
     [
       '10000',
       '987654321',
-      '12345678900',
-    ].forEach((qq) => {
+      '12345678900'
+    ].forEach(qq => {
       assert.ok(RegExps.qq.test(qq));
     });
     [
       '1000',
       '0987654321',
-      '123456789000',
-    ].forEach((qq) => {
+      '123456789000'
+    ].forEach(qq => {
       assert.ok(!RegExps.qq.test(qq));
     });
   });
@@ -50,7 +54,9 @@ describe('正则表达式', () => {
     [
       '5',
       '+5',
+      '+5.0',
       '-5',
+      '-5.0',
       '.5',
       '0.5',
       '1.5',
@@ -58,26 +64,38 @@ describe('正则表达式', () => {
       '+.5',
       '10000',
       '10000.',
-      '100.00',
-    ].forEach((number) => {
+      '100.00'
+    ].forEach(number => {
       assert.ok(RegExps.number.test(number));
     });
     [
       '.5.5',
       '1.5.5',
-    ].forEach((number) => {
+      '--0.5',
+      '++0.5',
+      '0.+5',
+      '0.5+',
+      '+-0.5'
+    ].forEach(number => {
       assert.ok(!RegExps.number.test(number));
     });
   });
 
   it('匹配邮箱', () => {
     [
+      '12asdf345@163.com',
       '12345@qq.com',
+      'balabala@163.com',
+      'wx.wagn@email.cn',
+      'wx.wang@al.com.cn'
     ].forEach((value) => {
       assert.ok(RegExps.email.test(value));
     });
     [
+      '_123sdf@qq.com',
       '12345#qq.com',
+      ' 123asdf@126.com'
+
     ].forEach((value) => {
       assert.ok(!RegExps.email.test(value));
     });
@@ -86,11 +104,14 @@ describe('正则表达式', () => {
   it('匹配 url', () => {
     [
       'https://www.qq.com',
+      'http://i.wang.com'
     ].forEach((value) => {
       assert.ok(RegExps.url.test(value));
     });
     [
-      'http//www.qq.com',
+      'httpss://www.baidu.com',
+      'awed://www.baidu.com',
+      'http//www.qq.com'
     ].forEach((value) => {
       assert.ok(!RegExps.url.test(value));
     });
@@ -98,31 +119,40 @@ describe('正则表达式', () => {
 
   it('匹配 IPv4', () => {
     [
+      '0.0.0.0',
       '127.0.0.1',
+      '1.1.1.255',
+      '255.255.255.254'
     ].forEach((value) => {
       assert.ok(RegExps.ipv4.test(value));
     });
     [
+      '0.192.167.0',
       '256.0.0.0',
+      '255.255.255.256',
+      '192.168.0.1255'
     ].forEach((value) => {
       assert.ok(!RegExps.ipv4.test(value));
     });
   });
-
   it('匹配身份证号码', () => {
     [
       '350301198906180060',
+      '142202199801254866',
+      '14245219980125486X',
+      '11255219980125486Y'
     ].forEach((value) => {
       assert.ok(RegExps.idcard.test(value));
     });
     [
+      '142202189201103589X',
       '350301298906180060',
       '350301298906310060',
+      '350301201902088945',
       '35030129890618006Y',
       '3503012989061800666',
     ].forEach((value) => {
       assert.ok(!RegExps.idcard.test(value));
     });
   });
-
 });

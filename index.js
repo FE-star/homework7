b1291bb3ba5dcccb91548799e716bca28cd4da97
icebox1234 <464752812@qq.com>
2022-09-09 08:54:39 +0800

    feat: 验证身份证能够办理的年份

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ module.exports = {
   email: /^\w+@[A-Za-z0-9]+\.com$/,
   url: /^http[s]?:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+\.(org|com|cn|gov)$/,
   ipv4: /^((0|[1-9]|1\d{1,2}|2[0-4]\d|25[0-5])\.){3}(0|[1-9]|1\d{1,2}|2[0-4]\d|25[0-5])$/,
-  idcard: /^\d{6}\d{4}(((0[13578]|1[02])([0-2]\d|3[01]))|((0[469]|11)([0-2]\d|30))|(02[0-2]\d))\d{3}[0-9X]{1}$/,
+  idcard: {
+    test: (value = '') => {
+      const reg = /^\d{6}\d{4}(((0[13578]|1[02])([0-2]\d|3[01]))|((0[469]|11)([0-2]\d|30))|(02[0-2]\d))\d{3}[0-9X]{1}$/;
+      if (reg.test(value)) {
+        const year = parseInt(value.slice(6, 10), 10);
+        const curMaxIdYear = new Date().getFullYear() - 18;
+        return !(year > curMaxIdYear);
+      }
+      return false;
+    }
+  },
 };
